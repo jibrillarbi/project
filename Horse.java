d8125b8d32d8f97b50a4a9e43dcab9f7a6fa659e
jibrillarbi <jibrillarbi.pro@gmail.com>
2024-04-26 13:23:44 +0100

    Finished accessor methods and methods

--- Horse.java
+++ Horse.java
@@ -5,72 +5,70 @@
  * @author Jibril
  * @version (a version number or a date)
  */
-public class Horse
-{
-    //Fields of class Horse
-    
-    
-      
-    //Constructor of class Horse
-    /**
-     * Constructor for objects of class Horse
-     */
-    public Horse(char horseSymbol, String horseName, double horseConfidence)
-    {
-       
-    }
-    
-    
-    
-    //Other methods of class Horse
-    public void fall()
-    {
-        
-    }
-    
-    public double getConfidence()
-    {
-        
-    }
-    
-    public int getDistanceTravelled()
-    {
-        
-    }
-    
-    public String getName()
-    {
-        
-    }
-    
-    public char getSymbol()
-    {
-        
-    }
-    
-    public void goBackToStart()
-    {
-        
-    }
-    
-    public boolean hasFallen()
-    {
-        
+public class Horse {
+    private String name;
+    private char symbol;
+    private int distanceTravelled;
+    private boolean hasFallen;
+    private double confidence;
+
+    // constructor
+    public Horse(char horseSymbol, String horseName, double horseConfidence) {
+        this.symbol = horseSymbol;
+        this.name = horseName;
+        this.confidence = horseConfidence;
+        this.distanceTravelled = 0;  // Default starting distance
+        this.hasFallen = false;      // Default fallen state
     }
 
-    public void moveForward()
-    {
-        
+    // accessor methods
+
+    //getters
+    public String getName() {
+        return name;
     }
 
-    public void setConfidence(double newConfidence)
-    {
-        
+    public char getSymbol() {
+        return symbol;
     }
-    
-    public void setSymbol(char newSymbol)
-    {
-        
+
+    public int getDistanceTravelled() {
+        return distanceTravelled;
     }
-    
+
+    public double getConfidence() {
+        return confidence;
+    }
+
+    public boolean hasFallen() {
+        return hasFallen;
+    }
+
+    // setters
+    public void setSymbol(char newSymbol) {
+        this.symbol = newSymbol;
+    }
+
+    public void setConfidence(double newConfidence) {
+        if (newConfidence >= 0.0 && newConfidence <= 1.0) {
+            this.confidence = newConfidence;
+        } else {
+            System.out.println("Confidence must be between 0.0 and 1.0.");
+        }
+    }
+
+    // methods
+    public void fall() {
+        hasFallen = true;
+    }
+
+    public void moveForward() {
+        if (!hasFallen) {
+            distanceTravelled++;
+        }
+    }
+
+    public void goBackToStart() {
+        distanceTravelled = 0;
+    }
 }
